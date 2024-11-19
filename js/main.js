efaf7cf6db18ef7ddbe2dffb10d8e4d5b9b9f505
anouther <155570107+anouther@users.noreply.github.com>
2024-11-19 21:48:36 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@
       const c = setTimeout(() => {
         document.body.classList.remove("not-loaded");
 
-        const titles = ('Thankyou').split('');
+        const titles = ('Far, i love u n thankyou').split('');
         const titleElement = document.getElementById('title');
         let index = 0;
 
@@ -21,7 +21,7 @@
           } else {
             // Setelah teks utama selesai, tambahkan subtitle kecil
             const subtitle = document.createElement('p');
-            subtitle.innerText = "thankyou for being the one person who understood me when no one else did and thankyou for holding me while I cry when everyone else abandoned me. thank you for everything far, iloveusomuch";
+            subtitle.innerText = "thankyou for being the one person who understood me when no one else did and thankyou for holding me while I cry when everyone else abandoned me. thank you for everything far, iloveusomuch — Nathan";
             subtitle.style.fontSize = '20px'; // Ukuran font lebih kecil
             subtitle.style.marginTop = '2px'; // Spasi antara teks utama dan subtitle
             subtitle.style.color = '#fff'; // Warna abu-abu untuk teks subtitle
